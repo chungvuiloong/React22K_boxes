bc0c4d9d8fec97ece04e30606d59e0c8be7e7a72
Jere <35882808+jirimicvl@users.noreply.github.com>
2022-03-22 12:07:24 +0200

    editted out helloworld

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 
-export default function App() {
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
         
         <p>Jere Jere Jere Jere</p>
         <h1>sdfdsfdsdsfdsfds</h1>
+
       </header>
     </div>
   );
-}
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -3,10 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// import Helloworld from './Helloworld';
 
 ReactDOM.render(<App />,
   document.getElementById('root')
 );
+
+// ReactDOM.render(<Helloworld />,
+//   document.getElementById('root')
+// );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
