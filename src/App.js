66f47cb3f8d3b787c9d49c33808fa6e5eee43f37
Jere <35882808+jirimicvl@users.noreply.github.com>
2022-03-22 12:36:26 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 
-const name = 'MrJay';
+const name  = 'MrJay';
+const year  = new Date().getFullYear();
+const month = new Date().getMonth() + 1;
+const date  = new Date().getDate();
+const started_year = 2016;
 
 const App = () => {
   return (
@@ -9,7 +13,8 @@ const App = () => {
       <header className="App-header">
         
         <p>Jere Jere Jere Jere</p>
-        <h1>Hellow World, {name}</h1>
+        <h1>Hello World, {name}, {year}, {month}, {date}</h1>
+        <p>I started coding {year - started_year} years ago</p>
 
       </header>
     </div>
