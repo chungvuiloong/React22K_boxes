46caa9a6cf2e5c77809f99224ad2506774806d3a
Jere <35882808+jirimicvl@users.noreply.github.com>
2022-03-22 10:52:39 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> Jere Jere Jere Jere
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Jereism
-        </a>
+        
+        <p>Jere Jere Jere Jere</p>
+        <h1>sdfdsfdsdsfdsfds</h1>
       </header>
     </div>
   );
 }
-
-export default App;
