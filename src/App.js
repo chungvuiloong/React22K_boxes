b4e08b0045870d30f865021b4e4184b57bdfa618
Jere <35882808+jirimicvl@users.noreply.github.com>
2022-03-22 14:45:38 +0200

    got langauges working

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ import Main from './Main';
 import Box from './Box';
 
 const persons = [
-  {name:"Maria", title:"CEO", age:"29", languages:[{name:'English', id: 1}, {name:'Russian', id: 2}]},
-  {name:"Kati", title:"Developer", age:"25", languages:[{name:'English', id: 1}, {name:'Russian', id: 2}]},
-  {name:"Karin", title:"Designer", age:"55", languages:[{name:'English', id: 1}, , {name:'Russian', id: 2}]},
-  {name:"Jere", title:"CEO", age:"29", languages:[{name:'English', id:1}, {name:'Russian', id:2}, {name:'Japanese', id:3}]},
+  {name:"Maria", title:"CEO", age:"29",       languages:[{name:'English', id: 1}, {name:'Russian', id: 2}]},
+  {name:"Kati", title:"Developer", age:"25",  languages:[{name:'English', id: 1}, {name:'Russian', id: 2}]},
+  {name:"Karin", title:"Designer", age:"55",  languages:[{name:'English', id: 1}, , {name:'Russian', id: 2}]},
+  {name:"Jere", title:"CEO", age:"29",      languages:[{name:'English', id:1}, {name:'Russian', id:2}, {name:'Japanese', id:3}]},
   {name:"Eric", title:"CFO", age:"20", languages:[{name:'English', id: 1}, , {name:'Russian', id: 2}]},
   {name:"Niina", title:"CO-CEO", age:"25", languages:[{name:'English', id: 1}, , {name:'Russian', id: 2}, {name: 'Spanish', id:3}]},
 ];
