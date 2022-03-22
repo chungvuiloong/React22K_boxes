add4015490d6e96e03800f8ab7322f1a3bdc1cd1
Jere <35882808+jirimicvl@users.noreply.github.com>
2022-03-22 13:35:04 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -2,12 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './Header';
 import Footer from './Footer';
-
-const name  = 'MrJay';
-const year  = new Date().getFullYear();
-const month = new Date().getMonth() + 1;
-const date  = new Date().getDate();
-const started_year = 2016;
+import Main from './Main';
 
 const App = () => {
   return (
@@ -16,11 +11,11 @@ const App = () => {
         <Header />
        
       </header>
-      <main>
-        <p>Jere Jere Jere Jere</p>
-        <h1>Hello World, {name}, {year}, {month}, {date}</h1>
-        <p>I started coding {year - started_year} years ago</p>
+      
+      <main className="App-main">
+        <Main />
       </main>
+
       <footer className="App-footer">
         <Footer />
       </footer>
---
+++ src/Main.js
@@ -0,0 +1,41 @@
+import './App.css';
+
+const name  = 'MrJay';
+const year  = new Date().getFullYear();
+const month = new Date().getMonth() + 1;
+const date  = new Date().getDate();
+const started_year = 2016;
+
+const Main = () => {
+  return (
+    <div>
+{/*  
+        <p>Jere Jere Jere Jere</p>
+        <h1>Hello World, {name}, {year}, {month}, {date}</h1>
+        <p>I started coding {year - started_year} years ago</p>
+         */}
+        <div className="container"> 
+            <div className="cards">
+                <h4>Maria</h4>
+                <div>Title: CEO</div>
+                <div>Age: 20</div>
+            </div>
+
+            <div className="cards">
+                <h4>Kati</h4>
+                <div>Tile: Designer</div>
+                <div>Age: 25</div>
+            </div>
+            
+            <div className="cards">
+                <h4>Karin</h4>
+                <div>Title: Designer</div>
+                <div>Age: 35</div>
+            </div>
+        </div>
+
+    </div>
+  );
+};
+
+export default Main;
