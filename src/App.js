e20dca4b35041029ab5b96e4e6b9275a43d2158d
Jere <35882808+jirimicvl@users.noreply.github.com>
2022-03-22 12:14:24 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 
+const name = 'MrJay';
+
 const App = () => {
   return (
     <div className="App">
       <header className="App-header">
         
         <p>Jere Jere Jere Jere</p>
-        <h1>sdfdsfdsdsfdsfds</h1>
+        <h1>Hellow World, {name}</h1>
 
       </header>
     </div>
